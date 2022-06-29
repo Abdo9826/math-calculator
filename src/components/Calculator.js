1e83087be505c0a45e563888e9b1d05b6ba6e04a
Abdo9826 <102296263+Abdo9826@users.noreply.github.com>
2022-06-30 00:36:17 +0300

    add components

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,36 +1,72 @@
 import React, { Component } from 'react';
+import calculate from './logic/calculate';
 
 class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: '',
+      next: '',
+      operation: '',
+    };
+    this.handleClick = this.handleClick.bind(this);
   }
 
+  handleClick = (e) => {
+    const buttonName = e.target.name;
+    this.setState((prevState) => ({
+      ...calculate(prevState, buttonName),
+    }));
+  };
+
   render() {
+    const { total, next, operation } = this.state;
     return (
       <div className="container my-4">
         <div className="calculator card">
-          <input
-            type="text"
-            className="calculator-screen z-depth-1"
-            value=""
+          {/* <input
+            type='text'
+            className='calculator-screen z-depth-1'
+            value='12'
             disabled
-          />
+          /> */}
+          <div className="calculator-screen z-depth-1">
+            <span>{next || operation || total || ''}</span>
+          </div>
 
           <div className="calculator-keys">
-            <button type="button" className="operator btn btn-info" value="+">
+            <button
+              type="button"
+              className="operator btn btn-info"
+              name="+"
+              value="+"
+              onClick={this.handleClick}
+            >
               +
             </button>
-            <button type="button" className="operator btn btn-info" value="-">
+            <button
+              type="button"
+              className="operator btn btn-info"
+              name="-"
+              value="-"
+              onClick={this.handleClick}
+            >
               -
             </button>
-            <button type="button" className="operator btn btn-info" value="*">
+            <button
+              type="button"
+              className="operator btn btn-info"
+              name="x"
+              value="x"
+              onClick={this.handleClick}
+            >
               &times;
             </button>
             <button
               type="button"
               className="operator btn btn-info orange"
               value="/"
+              onClick={this.handleClick}
             >
               &divide;
             </button>
@@ -39,6 +75,8 @@ class Calculator extends Component {
               type="button"
               value="7"
               className="btn btn-light waves-effect"
+              name="7"
+              onClick={this.handleClick}
             >
               7
             </button>
@@ -46,6 +84,8 @@ class Calculator extends Component {
               type="button"
               value="8"
               className="btn btn-light waves-effect"
+              name="8"
+              onClick={this.handleClick}
             >
               8
             </button>
@@ -53,6 +93,8 @@ class Calculator extends Component {
               type="button"
               value="9"
               className="btn btn-light waves-effect"
+              name="9"
+              onClick={this.handleClick}
             >
               9
             </button>
@@ -61,6 +103,8 @@ class Calculator extends Component {
               type="button"
               value="4"
               className="btn btn-light waves-effect"
+              name="4"
+              onClick={this.handleClick}
             >
               4
             </button>
@@ -68,6 +112,8 @@ class Calculator extends Component {
               type="button"
               value="5"
               className="btn btn-light waves-effect"
+              name="5"
+              onClick={this.handleClick}
             >
               5
             </button>
@@ -75,6 +121,8 @@ class Calculator extends Component {
               type="button"
               value="6"
               className="btn btn-light waves-effect"
+              name="6"
+              onClick={this.handleClick}
             >
               6
             </button>
@@ -83,6 +131,8 @@ class Calculator extends Component {
               type="button"
               value="1"
               className="btn btn-light waves-effect"
+              name="1"
+              onClick={this.handleClick}
             >
               1
             </button>
@@ -90,6 +140,8 @@ class Calculator extends Component {
               type="button"
               value="2"
               className="btn btn-light waves-effect"
+              name="2"
+              onClick={this.handleClick}
             >
               2
             </button>
@@ -97,6 +149,8 @@ class Calculator extends Component {
               type="button"
               value="3"
               className="btn btn-light waves-effect"
+              name="3"
+              onClick={this.handleClick}
             >
               3
             </button>
@@ -105,6 +159,8 @@ class Calculator extends Component {
               type="button"
               value="0"
               className="btn btn-light waves-effect"
+              name="0"
+              onClick={this.handleClick}
             >
               0
             </button>
@@ -112,13 +168,17 @@ class Calculator extends Component {
               type="button"
               className="decimal function btn btn-secondary"
               value="."
+              name="."
+              onClick={this.handleClick}
             >
               .
             </button>
             <button
               type="button"
               className="all-clear function btn btn-danger btn-sm"
-              value="all-clear"
+              value="AC"
+              name="AC"
+              onClick={this.handleClick}
             >
               AC
             </button>
@@ -127,6 +187,8 @@ class Calculator extends Component {
               type="button"
               className="equal-sign operator btn btn-default"
               value="="
+              name="="
+              onClick={this.handleClick}
             >
               =
             </button>
