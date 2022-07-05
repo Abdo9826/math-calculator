9e636c69a5ee70cbfaf96255d8408acbb7ac8ec9
Abdo9826 <102296263+Abdo9826@users.noreply.github.com>
2022-07-05 21:13:28 +0300

    Add nav bar with router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { Route, Switch } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Nav from './components/nav';
+import Home from './components/home';
+import Quote from './components/Quote';
 
 class App extends Component {
   constructor(props) {
@@ -10,8 +15,29 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Calculator />
+        <Router>
+          <Nav />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/quote" element={<Quote />} />
+            <Route exact path="/calculator" element={<Calculator />} />
+          </Routes>
+        </Router>
       </div>
+      // <div>
+      //   <Nav />
+      //   <Switch>
+      //     <Route path="/" exact>
+      //       <Home />
+      //     </Route>
+      //     <Route path="/calculator">
+      //       <Calculator />
+      //     </Route>
+      //     <Route path="/quote">
+      //       <Quote />
+      //     </Route>
+      //   </Switch>
+      // </div>
     );
   }
 }
